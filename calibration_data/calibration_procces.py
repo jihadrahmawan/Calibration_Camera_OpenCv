0a92782f5dab4a0711fb55eba21e2f93b0b09b46
Jihad Rahmawan <jihadrahmawan20@gmail.com>
2021-08-27 15:44:58 +0900

    Add files via upload

---
+++ calibration_data/calibration_procces.py
@@ -0,0 +1,132 @@
+import numpy as np
+import cv2
+import glob
+import yaml
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((9*6,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob('*.png')
+index=0
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (9,6), corners2,ret)
+        cv2.imshow('img',img)
+        cv2.waitKey(500)
+        index=index+1
+        filename = "calibrated_image_%d.jpg"%index
+        cv2.imwrite(filename, img)
+
+cv2.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+
+print ("ret", ret)
+print ("mtx", mtx)
+print ("dist", dist)
+print ("rvecs", rvecs)
+print ("tvecs", tvecs)
+
+data = {'mtx': np.asarray(mtx).tolist(),
+        'dist': np.asarray(dist).tolist(),
+        'rvecs': np.asarray(rvecs).tolist(),
+        'tvecs': np.asarray(tvecs).tolist(),
+        }
+
+with open("calibraton_data_ELPUSBFHD01M-BFV.yaml", "w") as f:
+    yaml.dump(data, f)   
+
+'''
+file = open("calb_result.yaml")
+present = yaml.load(file, Loader=yaml.FullLoader)
+
+mtxs=np.asarray(present["mtx"])
+dists=np.asarray(present["dist"])
+        
+axis = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
+                   [0,0,-3],[0,3,-3],[3,3,-3],[3,0,-3] ])
+
+def draw(img, corners, imgpts):
+    imgpts = np.int32(imgpts).reshape(-1,2)
+
+    # draw ground floor in green
+    img = cv2.drawContours(img, [imgpts[:4]],-1,(0,255,0),-3)
+
+    # draw pillars in blue color
+    for i,j in zip(range(4),range(4,8)):
+        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
+
+    # draw top layer in red color
+    img = cv2.drawContours(img, [imgpts[4:]],-1,(0,0,255),3)
+
+    return img
+ 
+cap = cv2.VideoCapture(0)
+ 
+while (1):
+    ret, frame = cap.read()
+    img = frame
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray, (7,7),None)
+
+    if ret == True:
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+
+        # Find the rotation and translation vectors.
+        _, rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, corners2, mtxs, dists)
+
+        # project 3D points to image plane
+        imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
+
+        img = draw(img,corners2,imgpts)
+        cv2.imshow('img',img)
+        k = cv2.waitKey(1)
+        if k == 's':
+            cv2.imwrite(fname[:6]+'.png', img)
+    else:
+       continue
+
+cv2.destroyAllWindows()
+
+
+img = cv2.imread('WIN_20210204_13_27_09_Pro.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+# undistort
+dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+# crop the image
+x,y,w,h = roi
+dst = dst[y:y+h, x:x+w]
+cv2.imwrite('calibresult.png',dst)
+
+# undistort
+mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
+dst = cv2.remap(img,mapx,mapy,cv2.INTER_LINEAR)
+
+# crop the image
+x,y,w,h = roi
+dst = dst[y:y+h, x:x+w]
+cv2.imwrite('calibresult2.png',dst)
+'''
